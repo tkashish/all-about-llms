397e4aaccd03395d54c9626af58327f2b359b743
Kashish Tayal <ktkashish@gmail.com>
2026-04-27 17:05:39 -0700

    [Transformer] adding embedding layer

---
+++ src/transformer/embedding_table.py
@@ -0,0 +1,26 @@
+import math
+
+import torch
+from torch import nn
+
+
+class EmbeddingTable(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.table = nn.Parameter(torch.randn(vocab_size, d_model)/math.sqrt(d_model))
+        # dividing by sqrt of d_model keeps the variance after matmul roughly 1.
+        # after matmul we do multiplication and addition which would have increase variance.
+
+    """
+    B = Batch Size
+    T = Tokens per batch
+    D = Tensor per token
+    Input: 
+    x.shape = [B, T]
+    Output:
+    out.shape =  [B, T, D]
+    In this layer we want to add the vector for each token
+    """
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.table[x]
+
---
+++ src/transformer/model.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+import torch
+from torch import nn
+
+from transformer.embedding_table import EmbeddingTable
+
+
+@dataclass
+class HyperParams:
+    d_model: int
+    vocab_size: int
+
+class Model(nn.Module):
+    def __init__(self, params: HyperParams):
+        super().__init__()
+        self.embeddings_table = EmbeddingTable(params.d_model, params.vocab_size)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.embeddings_table(x)
+        return x
+
+
+if __name__ == '__main__':
+    params =HyperParams(
+        d_model=10,
+        vocab_size=1000
+    )
+    model = Model(params)
+    model.to("mps")
+    input = torch.arange(0,12).reshape(4,3)
+    input.to("mps")
+    out = model(input)
+    print(f"Input Shape: {input.shape}")
+    print(f"Output Shape: {out.shape}")
