39a07b3e6708d985383cf942446472e1588c537e
Kashish Tayal <ktkashish@gmail.com>
2026-04-27 20:13:09 -0700

    [Transformer] adding final layer to generate logits

--- src/transformer/model.py
+++ src/transformer/model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+import einops
 import torch
 from torch import nn
 
@@ -14,23 +15,28 @@ class HyperParams:
     num_heads: int
     max_seq_len: int
     d_ff: int
+    num_transformer_blocks: int
 
 class Model(nn.Module):
     def __init__(self, params: HyperParams):
         super().__init__()
         self.embeddings_table = EmbeddingTable(params.d_model, params.vocab_size)
-        self.transformer = Transformer(TransformerHyperParams(
+        self.transformers = nn.ModuleList([Transformer(TransformerHyperParams(
             d_model=params.d_model,
             num_heads =params.num_heads,
             max_seq_len=params.max_seq_len,
             d_ff=params.d_ff
-        ))
+        )) for _ in range(params.num_transformer_blocks)])
+        self.rms_norm = nn.RMSNorm(params.d_model)
+
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.embeddings_table(x)
-        x = self.transformer(x)
-        return x
-
+        for transformer in self.transformers:
+            x = transformer(x)
+        x = self.rms_norm(x)
+        logits = einops.einsum(x, self.embeddings_table.table.T, "b t d, d t_all -> b t t_all")
+        return logits
 
 if __name__ == '__main__':
     params = HyperParams(
@@ -38,7 +44,8 @@ if __name__ == '__main__':
         vocab_size=1000,
         num_heads = 2,
         max_seq_len=10,
-        d_ff=40
+        d_ff=40,
+        num_transformer_blocks=10
     )
     model = Model(params)
     model.to("mps")
--- src/transformer/transformer.py
+++ src/transformer/transformer.py
@@ -18,9 +18,10 @@ class Transformer(nn.Module):
         super().__init__()
         self.attention = Attention(params.d_model, params.num_heads, params.max_seq_len)
         self.mlp = MLP(params.d_model, params.d_ff)
-        self.rms_norm = nn.RMSNorm(params.d_model)
+        self.rms_norm1 = nn.RMSNorm(params.d_model)
+        self.rms_norm2 = nn.RMSNorm(params.d_model)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x + self.attention(self.rms_norm(x))
-        x = x + self.mlp(self.rms_norm(x))
+        x = x + self.attention(self.rms_norm1(x))
+        x = x + self.mlp(self.rms_norm2(x))
         return x
